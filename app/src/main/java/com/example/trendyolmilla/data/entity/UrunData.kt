aed744fbf115e086fc30333890c62a3964ab1cc8
ebruaeo <ebruaeo@hotmail.com>
2024-03-04 15:31:38 +0300

    Products data class

---
+++ app/src/main/java/com/example/trendyolmilla/data/entity/UrunData.kt
@@ -0,0 +1,6 @@
+package com.example.trendyolmilla.data.entity
+
+import java.io.Serializable
+
+data class UrunData(var image: Int, var brand: String, var description: String, var price: Double): Serializable {
+}
