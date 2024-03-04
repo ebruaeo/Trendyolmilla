2fc5f07076a1996563509724d178ac8c791abb19
ebruaeo <ebruaeo@hotmail.com>
2024-03-04 15:34:12 +0300

    binding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,9 @@ android {
     namespace = "com.example.trendyolmilla"
     compileSdk = 34
 
+    buildFeatures{
+        viewBinding=true
+    }
     defaultConfig {
         applicationId = "com.example.trendyolmilla"
         minSdk = 24
